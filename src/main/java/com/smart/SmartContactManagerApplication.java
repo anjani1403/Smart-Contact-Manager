2ac13ff2cdba0c9dab34b55f4b9474db4b052f51
Anjani Manthapurwar <anjanimanthapurwar1403@gmail.com>
2025-07-10 21:51:13 +0530

    Configured Project for WAR deployment

--- src/main/java/com/smart/SmartContactManagerApplication.java
+++ src/main/java/com/smart/SmartContactManagerApplication.java
@@ -2,12 +2,19 @@ package com.smart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SmartContactManagerApplication {
+public class SmartContactManagerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartContactManagerApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SmartContactManagerApplication.class);
 	}
 
 }
